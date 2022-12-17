4c00b05a31002846e58c76694d79ebf917bd1233
Matheus Felipe Ruiz Gonçalves <matheus57.ruiz100@gmail.com>
2022-12-17 06:14:59 -0300

    Arquivo HTML

---
+++ teoria.js
@@ -0,0 +1,7 @@
+/* ### Pull Request (PR):
+    - É uma solicitação de fusão de uma determinada branch com a branch principal (Main ou Master), ou seja,
+    é como um merge, mas que pede autorização;
+
+    >> 
+
+*/
